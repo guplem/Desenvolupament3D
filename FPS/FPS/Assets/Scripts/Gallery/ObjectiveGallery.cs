61c33ebb5f2e5bde40c1414d174171a6922496c2
gillemp <guplem@gmail.com>
2019-10-02 20:50:42 +0200

    Gallery ready

---
+++ FPS/FPS/Assets/Scripts/Gallery/ObjectiveGallery.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveGallery : MonoBehaviour
+{
+    private ShootingGallery sg;
+        private int hp = 3;
+    
+    public void StartMatch(ShootingGallery sg)
+    {
+        gameObject.SetActive(true);
+        this.sg = sg;
+        
+        //TODO: animations and everything...
+    }
+
+    public void Hit()
+    {
+        hp--;
+
+        if (hp < 0)
+        {
+            sg.score += 100;
+            sg.NextObjective();
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ FPS/FPS/Assets/Scripts/Gallery/ShootingGallery.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ShootingGallery : MonoBehaviour
+{
+    [SerializeField] private TMP_Text scoreCount;
+    public int score;
+    private int objectiveIndex;
+
+    [SerializeField] private ObjectiveGallery[] galleryObjectives;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Entering Gallery");
+            
+            scoreCount.gameObject.SetActive(true);
+            score = 0;
+            objectiveIndex = 0;
+            NextObjective();
+        }
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Exiting Gallery");
+            scoreCount.gameObject.SetActive(false);
+
+            foreach (ObjectiveGallery objective in galleryObjectives)
+                objective.gameObject.SetActive(false);
+
+        }
+    }
+
+
+    public void NextObjective()
+    {
+        galleryObjectives[objectiveIndex].StartMatch(this);
+        objectiveIndex++;
+    }
+}

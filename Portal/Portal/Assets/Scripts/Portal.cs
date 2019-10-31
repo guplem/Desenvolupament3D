ba713c87dc0b42241d2d3283675e28c54639639e
gillemp <guplem@gmail.com>
2019-10-31 19:11:13 +0100

    Portal improved

--- Portal/Portal/Assets/Scripts/Portal.cs
+++ Portal/Portal/Assets/Scripts/Portal.cs
@@ -14,19 +14,20 @@ public class Portal : MonoBehaviour
     
     void Update ()
     {
-        Vector3 l_ReflectedVector=m_PlayerCamera.position-transform.position;
-        l_ReflectedVector=transform.position-l_ReflectedVector;
-        Debug.DrawLine(transform.position , l_ReflectedVector, Color.red);
+        Vector3 l_EulerAngles=transform.rotation.eulerAngles;
+        Quaternion l_Rotation=Quaternion.Euler(l_EulerAngles.x, l_EulerAngles.y+180.0f, l_EulerAngles.z);
+        Matrix4x4 l_WorldMatrix=Matrix4x4.TRS(transform.position, l_Rotation, transform.localScale);
+        Vector3 l_ReflectedPosition=l_WorldMatrix.inverse.MultiplyPoint3x4(m_PlayerCamera.position);
+        Vector3 l_ReflectedDirection=l_WorldMatrix.inverse.MultiplyVector(m_PlayerCamera.forward);
+        m_MirrorPortal.m_PortalCamera.transform.position=m_MirrorPortal.transform.TransformPoint(l_ReflectedPosition);
+        m_MirrorPortal.m_PortalCamera.transform.forward=m_MirrorPortal.transform.TransformDirection(l_ReflectedDirection);
+        m_PortalCamera.nearClipPlane=Vector3.Distance(m_PortalCamera.transform.position, this.transform.position)+m_NearClipOffset;
         
-        l_ReflectedVector=transform.InverseTransformPoint(l_ReflectedVector);
-        m_MirrorPortal.m_PortalCamera.transform.position = m_MirrorPortal.transform.TransformPoint(l_ReflectedVector);
-        m_PortalCamera.transform.forward=(transform.position-m_PortalCamera.transform.position).normalized;
-        m_PortalCamera.nearClipPlane = Vector3.Distance(m_PortalCamera.transform.position, this.transform.position)+m_NearClipOffset;
         
         //FOV
-        Vector3 l_PlayerToPortal=transform.position-m_PlayerCamera.position;
+        /*Vector3 l_PlayerToPortal=transform.position-m_PlayerCamera.position;
         float l_Distance=l_PlayerToPortal.magnitude;
         float l_Pct=1.0f-Mathf.Min(l_Distance/m_MaxFOVDistance, 1.0f);
-        m_MirrorPortal.m_PortalCamera.fieldOfView=Mathf.Lerp(m_MinFOV, m_maxFov, l_Pct);
+        m_MirrorPortal.m_PortalCamera.fieldOfView=Mathf.Lerp(m_MinFOV, m_maxFov, l_Pct);*/
     }
 }

ee9ca936c7d1cbb4ff382c4dc05f683e0cc2f61b
guplem <guplem@gmail.com>
2019-11-12 22:19:53 +0100

    Weird stuff missing

--- Portal/Portal/Assets/Scripts/Player/PlayerManager.cs
+++ Portal/Portal/Assets/Scripts/Player/PlayerManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using TreeEditor;
 using UnityEditor;
 using UnityEngine;
 
@@ -9,12 +10,23 @@ public class PlayerManager : MonoBehaviour
 {
 
     [SerializeField] public Camera playerCamera;
+    [SerializeField] public GameObject portalPlaceHolder;
 
     private void Update()
     {
 
         if (Input.GetButtonDown("Fire1") && CanShoot())
             Shoot();
+            
+        Vector3 playerCamPos = playerCamera.transform.position;
+        Ray ray=new Ray(playerCamPos, transform.position-playerCamPos);
+        RaycastHit raycastHit;
+
+        if (Physics.Raycast(ray, out raycastHit))
+        {
+            portalPlaceHolder.transform.position = raycastHit.point;
+            Debug.Log(raycastHit.collider.gameObject.name);            
+        }
     }
 
 

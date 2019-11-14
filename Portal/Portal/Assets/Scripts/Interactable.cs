2cce4bac0196005942706db5595bace75509c9d9
guplem <guplem@gmail.com>
2019-11-14 04:41:45 +0100

    Cleaning

---
+++ Portal/Portal/Assets/Scripts/Interactable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UIElements;
+
+[RequireComponent(typeof(Collider))]
+public class Interactable : MonoBehaviour
+{
+    public UnityEvent OnStartInteract;
+    public UnityEvent OnEndInteract;
+
+    [SerializeField] private bool interactableByUser = true;
+    [SerializeField] private bool interactableByTrigger = false;
+    
+    private void OnTriggerStay(Collider other)
+    {
+        if (!interactableByUser) 
+            return;
+        
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                OnStartInteract.Invoke();
+            } 
+            else if (Input.GetKeyUp(KeyCode.E))
+            {
+                OnEndInteract.Invoke();
+            }
+        }
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!interactableByTrigger) 
+            return;
+        
+        OnStartInteract.Invoke();
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (!interactableByTrigger) 
+            return;
+        
+        OnEndInteract.Invoke();
+    }
+}

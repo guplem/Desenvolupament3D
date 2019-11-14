31d1c7c7f0c717d9e4ccc8bd19ce6db9c9905466
guplem <guplem@gmail.com>
2019-11-14 03:12:29 +0100

    Turrets

---
+++ Portal/Portal/Assets/TurretLaserBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretLaserBehaviour : MonoBehaviour, LaserBehaviour
+{
+    public void OnHitStart(Vector3 woldPosition)
+    {
+        Destroy(gameObject);
+    }
+
+    public void OnHitEnd()
+    {
+        
+    }
+}

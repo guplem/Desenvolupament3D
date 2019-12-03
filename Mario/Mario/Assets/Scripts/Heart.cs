3c9cdb356594fadfb134f84552429423b13a2730
guplem <guplem@gmail.com>
2019-12-03 12:46:24 +0100

    UI completed and heart added

---
+++ Mario/Mario/Assets/Scripts/Heart.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heart : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerManager pm = other.GetComponent<PlayerManager>();
+        if (pm == null) return;
+        pm.health++;
+        Destroy(gameObject);
+    }
+}
